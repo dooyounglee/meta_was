c46b3ba485e4e83a34079ff76559cdbcc1651113
doo <gostbaducking1@nate.com>
2024-11-28 07:55:16 +0900

    feat: add NumberUtil, StringUtil

---
+++ src/main/java/com/meta/cmm/util/NumberUtil.java
@@ -0,0 +1,20 @@
+package com.meta.cmm.util;
+
+public class NumberUtil {
+    
+    public static double round(double num, int digit) {
+		return Math.round(num * Math.pow(10, digit)) / Math.pow(10, digit);
+	}
+	
+	public static double ceil(double num, int digit) {
+		return Math.ceil(num * Math.pow(10, digit)) / Math.pow(10, digit);
+	}
+	
+	public static double floor(double num, int digit) {
+		return Math.floor(num * Math.pow(10, digit)) / Math.pow(10, digit);
+	}
+	
+	public static String lpad(int num, int length) {
+		return StringUtil.lpad(String.valueOf(num), length, "0");
+	}
+}
