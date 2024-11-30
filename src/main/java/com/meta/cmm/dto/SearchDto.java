d7beb9797ef87e05eddc883eeda60a9a5fe6f409
doo <gostbaducking1@nate.com>
2024-11-30 22:02:33 +0900

    feat: add SearchDto

---
+++ src/main/java/com/meta/cmm/dto/SearchDto.java
@@ -0,0 +1,18 @@
+package com.meta.cmm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+public class SearchDto {
+	
+	private String searchTyp;
+	private String searchWrd;
+}
