984681a12b198529572e956000668a28830f01b2
doo <gostbaducking1@nate.com>
2024-12-17 23:47:33 +0900

    fix: filerequest extends filebasedto

--- src/main/java/com/meta/cmm/file/controller/request/FileRequest.java
+++ src/main/java/com/meta/cmm/file/controller/request/FileRequest.java
@@ -1,13 +1,20 @@
 package com.meta.cmm.file.controller.request;
 
+import com.meta.cmm.dto.FileBaseDto;
 import com.meta.cmm.file.domain.AttachFile;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-@Getter
-@Builder
-public class FileRequest {
+@Data
+@EqualsAndHashCode(callSuper = true) // 부모 클래스에 필드를 포함하는 역할
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileRequest extends FileBaseDto {
     
     private long atchFileId;
     private int fileSn;
