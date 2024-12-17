27c3f5df0342d3d68f36f9179f03f19044c24287
doo <gostbaducking1@nate.com>
2024-12-17 23:45:49 +0900

    feat: add saveAll

--- src/main/java/com/meta/cmm/file/controller/port/CmmFileService.java
+++ src/main/java/com/meta/cmm/file/controller/port/CmmFileService.java
@@ -1,6 +1,9 @@
 package com.meta.cmm.file.controller.port;
 
+import java.io.IOException;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.meta.cmm.file.domain.AttachFile;
 
@@ -8,4 +11,5 @@ public interface CmmFileService {
 
     List<AttachFile> list(long atchFileId);
     AttachFile download(AttachFile dto);
+    long saveAll(MultipartHttpServletRequest multipartHttpServletRequest) throws IllegalStateException, IOException;
 }
