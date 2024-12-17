e74e447f82775bbc784be9a678483b06d01726a3
doo <gostbaducking1@nate.com>
2024-12-17 23:50:00 +0900

    fix: @transactional제거(aop로 적용하도록 대체)

--- src/main/java/com/meta/usr/user/service/UsrUserServiceImpl.java
+++ src/main/java/com/meta/usr/user/service/UsrUserServiceImpl.java
@@ -2,8 +2,6 @@ package com.meta.usr.user.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +20,6 @@ import com.meta.usr.user.service.port.UsrUserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class UsrUserServiceImpl implements UsrUserService {
     
